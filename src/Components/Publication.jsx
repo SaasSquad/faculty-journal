0b2ee4d6ce3d5e025d32467b962df68e986f24a0
Samuel <samuelajala01@gmail.com>
2024-03-19 10:00:06 -0700

    chore: fix date position

--- src/Components/Publication.jsx
+++ src/Components/Publication.jsx
@@ -1,11 +1,11 @@
 const Publication = (props) => {
     return ( <>
     <div className='border-gray-500 mx-[5vw] p-4 md:p-8 border-4 mb-4'>
-      <h3 className="text-2xl mb-2">{props.title}</h3>
+      <h3 className="text-2xl md:text-3xl font-bold mb-2">{props.title}</h3>
       <p className="mb-3">{props.desc}</p>
-      <div className='flex justify-between mb-4'><p>Date Published: {props.date}</p>
+      <div className=' mb-4 flex place-content-end'>
       <p>Author: {props.author}</p></div>
-      <button className="px-6 py-2 bg-blue-900">Read</button>
+      <div className="flex justify-between"><p>Date Published: {props.date}</p><button className="px-6 py-2 bg-blue-900">Read</button></div>
     </div></> );
 }
  
