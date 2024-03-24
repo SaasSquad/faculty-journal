877d6f753a063c2e346e4910d16264d19df17289
devsolary <mathewsolomon91@gmail.com>
2024-03-24 04:10:27 -0700

    contact page edit

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,5 +1,26 @@
 const Contact = () => {
-    return ( <></> );
+    return ( <>
+    <div className="md:ml-80 mt-[40px] mx-[10vw]">
+                <h1 className="font-bold text-2xl mb-6 ">CONTACT</h1>
+            
+               <p>
+                    Prof. Nurudeen A. Raji, Mechanical Engineering<br />
+                    Department, Lagos State University, Nigeria.<br /><br />
+                    P.M.B. 1012, Epe, Lagos, Nigeria<br /><br />
+                    <span className="font-bold">Phone:</span> <br />
+                    +234-803-325-1143 <br />
+                    +234-813-353-32
+                </p>
+                <div className="mt-4">
+                    <p><span className="font-bold">Enquiries:</span> <a className="hover:text-green-600 ml-2" href="#">info.etrj@gmail.com</a></p>
+                    <p><span className="font-bold">Submissions:</span> <a className="hover:text-green-600 ml-2" href="#">etrjournal@gmail.com</a></p>
+                    <p><span className="font-bold">Editorial:</span> <a className="hover:text-green-600 ml-2" href="#">editor.etrj@gmail.com</a></p>
+                    <p><span className="font-bold">Subscription: </span> <a className="hover:text-green-600 ml-2" href="#">subscriptionetrj@gmail.com</a></p>
+
+                </div>
+
+            </div>
+    </> );
 }
  
 export default Contact;
