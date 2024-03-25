9c862594602675caf259466879ab561cdf69f4e6
Defdave <igedavid01@gmail.com>
2024-03-25 02:25:11 +0100

    refactor some part of the code and implement the delete and approve api endpoint

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 // const API_URL = 'https://busy-erin-centipede-yoke.cyclic.app';
 
-// const API_URL = 'https://faculty-journal-backend.onrender.com';
+const API_URL = 'https://faculty-journal-backend.onrender.com';
 
-const API_URL = 'http://localhost:3001';
+// const API_URL = 'http://localhost:3001';
 
 const api = axios.create({
   baseURL: API_URL,
